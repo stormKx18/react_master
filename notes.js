cf9b001f19b90713d9cafb5ae674868f566686b5
hirian <vicjaime922@gmail.com>
2024-08-17 18:39:45 -0600

    Added first commands

---
+++ notes.js
@@ -0,0 +1,61 @@
+/*
+Learn react book: 57
+Check Node.js version
+>>node -v
+
+New project
+>>npm init -y
+
+Install project dependecies:
+>>npm install
+
+Install Packages
+>>npm install package-name
+
+Remove a package
+>>npm remove package-name
+*/
+
+
+//Template strings - Accept multiline strings
+console.log(`${lastName}, ${firstName} ${middleName}`);
+
+console.log(`${lastName}, 
+            ${firstName} 
+            ${middleName}`);
+
+//Function declaration
+function logCompliment() {
+    console.log("You're doing great!");
+    }
+
+//Arrow function - the arrow points to what should be returned
+const lordify = firstName => `${firstName} of Canterbury`;
+console.log(lordify("Chris")) //Chris of Canterbury
+
+const lordify2 = (firstName, land) => `${firstName} of ${land}`;
+console.log(lordify2("Don", "Piscataway")) //Don of Piscataway
+
+//Multiline arrow function
+const lordify3 = (firstName, land) => { 
+    //...function content
+    return True
+};
+
+//Function that returns an object
+// wrap the object you’re returning with parentheses
+const person = (firstName, lastName) => ({
+    first: firstName,
+    last: lastName
+    });
+console.log(person("Flad", "Hanson"));
+
+//Spread operator:
+const peaks = ["Tallac", "Ralston", "Rose"];
+const canyons = ["Ward", "Blackwood"];
+const tahoe = [...peaks, ...canyons];
+
+//The spread operator can also be used to get the remaining items in the array:
+const lakes = ["Donner", "Marlette", "Fallen Leaf", "Cascade"];
+const [first, ...others] = lakes;
+console.log(others.join(", ")); // Marlette, Fallen Leaf, Cascade
