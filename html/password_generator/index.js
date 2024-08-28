e1267f047b22e4de7b29a2a72c3441eec40815d9
hirian <vicjaime922@gmail.com>
2024-08-27 23:54:08 -0600

    Added basic js functions

--- html/password_generator/index.js
+++ html/password_generator/index.js
@@ -1,1 +1,6 @@
-console.log("index.js")
+
+function copyText(option) {
+    navigator.clipboard.writeText("Geeksforgeeks is best learning platform.");
+    const commentEl = document.querySelector("#comment");
+    commentEl.textContent="Password copied to clipboard!"
+}
