66457fd65b69cfc10b92a150b980a31f6c4e2e45
hirian <vicjaime922@gmail.com>
2024-08-30 22:46:08 -0600

    Added js functions to give conversion functions

--- html/unit_conversion/index.js
+++ html/unit_conversion/index.js
@@ -1,6 +1,27 @@
 const convertEl = document.querySelector("#convert-btn");
 const inputEl = document.querySelector("#user-in");
+const lengthEl = document.querySelector("#length-p");
+const volumeEl = document.querySelector("#volume-p");
+const massEl = document.querySelector("#mass-p");
+const METER2FOOT = 3.28084;
+const LITER2GALLON = 0.264172;
+const KG2LB = 2.20462;
 
 convertEl.addEventListener("click", function () {
-  console.log(inputEl.value);
+  const quantity = inputEl.value;
+
+  // Length
+  lengthEl.textContent = `
+  ${quantity} meters = ${(quantity * METER2FOOT).toFixed(3)} feet |
+  ${quantity} feet = ${(quantity / METER2FOOT).toFixed(3)} meters`;
+
+  //Volume
+  volumeEl.textContent = `
+  ${quantity} liters = ${(quantity * LITER2GALLON).toFixed(3)} gallons |
+  ${quantity} gallons = ${(quantity / LITER2GALLON).toFixed(3)} liters`;
+
+  //Mass
+  massEl.textContent = `
+  ${quantity} kilos = ${(quantity * KG2LB).toFixed(3)} pounds |
+  ${quantity} pounds = ${(quantity / KG2LB).toFixed(3)} kilos`;
 });
