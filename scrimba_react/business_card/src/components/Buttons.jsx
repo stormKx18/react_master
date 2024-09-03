2b257a2d5730215d6ca4cb2f99433687ce1e0f9d
hirian <vicjaime922@gmail.com>
2024-09-03 16:36:46 -0600

    Added linkedin icons and css formatting

--- scrimba_react/business_card/src/components/Buttons.jsx
+++ scrimba_react/business_card/src/components/Buttons.jsx
@@ -1,8 +1,16 @@
+import mailIcon from "../assets/mail_icon.png";
+import linkedinIcon from "../assets/linkedin_icon.png";
 export default function Buttons() {
   return (
     <div className="contact-buttons">
-      <button>Email</button>
-      <button>Linkedin</button>
+      <button id="email-btn">
+        <img src={mailIcon} alt="Mail icon" />
+        <span>Email</span>
+      </button>
+      <button id="linkedin-btn">
+        <img src={linkedinIcon} alt="Linkedin icon" width="13px" />
+        <span>Linkedin</span>
+      </button>
     </div>
   );
 }
